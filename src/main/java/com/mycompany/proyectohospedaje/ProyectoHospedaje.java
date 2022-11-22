2565ae14461e3d1192c39132d9715a332fb91c40
LuisCm <altronservice80@gmail.com>
2022-11-21 19:23:57 -0500

    Primeros archivos

---
+++ src/main/java/com/mycompany/proyectohospedaje/ProyectoHospedaje.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.proyectohospedaje;
+
+/**
+ *
+ * @authors Cerron Medina Luis
+ * agreguen sus nombres muchachos xd
+ */
+public class ProyectoHospedaje {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
